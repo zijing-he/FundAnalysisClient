472e3dfe5fc54eadf9e3ba4282c6f59aeba4c9bf
Dy <daiyong@zju.edu.cn>
2021-03-03 20:35:15 +0800

    solve bug

---
+++ src/components/ControlPanel/sorted-list.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    表格
+  </div>
+</template>
+<script>
+
+export default {
+  name: "SortedList",
+  data() {
+    return {
+    };
+  },
+  components: {
+  },
+  methods: {
+  },
+  mounted() {},
+};
+</script>
+
+<style scoped>
+
+</style>
